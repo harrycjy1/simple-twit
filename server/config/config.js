cca74e76ffb443ad44de6c357a2ddf77f83e34ce
choddol <harrycjy123@gmail.com>
2019-06-04 14:27:26 +0900

    connet issue fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
-
 //데이터베이스 url
 module.exports = {
   //로컬 머신에 몽고 DB연결
-  'url':'mongodb+srv://admin-cho:test123@cluster0-isdzk.mongodb.net/mvc-twit'
+  url:
+    "mongodb://admin-cho:test123@cluster0-shard-00-00-isdzk.mongodb.net:27017,cluster0-shard-00-01-isdzk.mongodb.net:27017,cluster0-shard-00-02-isdzk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
