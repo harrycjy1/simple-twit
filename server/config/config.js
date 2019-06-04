fb67cc22f9bbe9bdf4368f5a3970d02ceb6d40bc
choddol <harrycjy123@gmail.com>
2019-06-04 14:58:56 +0900

    connect with correct DB

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@
 module.exports = {
   //로컬 머신에 몽고 DB연결
   url:
-    "mongodb://admin-cho:test123@cluster0-shard-00-00-isdzk.mongodb.net:27017,cluster0-shard-00-01-isdzk.mongodb.net:27017,cluster0-shard-00-02-isdzk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    "mongodb://admin-cho:test123@cluster0-shard-00-00-isdzk.mongodb.net:27017,cluster0-shard-00-01-isdzk.mongodb.net:27017,cluster0-shard-00-02-isdzk.mongodb.net:27017/twitterdb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
